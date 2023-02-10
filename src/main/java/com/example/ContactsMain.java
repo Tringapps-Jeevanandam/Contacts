6002b83e2f6d6c422b62956aa3858054084408cb
Tringapps-Jeevanandam <123439486+Tringapps-Jeevanandam@users.noreply.github.com>
2023-02-10 17:19:26 +0530

    Update ContactsMain.java

--- src/main/java/com/example/ContactsMain.java
+++ src/main/java/com/example/ContactsMain.java
@@ -24,6 +24,7 @@ class StoreContact {
     StoreContact() {
         head = null;
     }
+
     public static final Logger Log = Logger.getLogger("InfoLogging");
 
     public void addContact(String name, long phNo, String email) {
@@ -49,16 +50,16 @@ class StoreContact {
         prev.next = null;
     }
 
-    public void delete(int phNo) {
+    public void delete(String name) {
         if (this.head == null) {
             throw new IndexOutOfBoundsException();
         }
-        if (this.head.phNo == phNo) {
+        if (this.head.name.equals(name)) {
             this.head = this.head.next;
         }
         Contact temp = head;
         Contact prev = null;
-        while (temp.phNo != phNo) {
+        while (!temp.name.equals(name)) {
             if (temp.next != null) {
                 prev = temp;
                 temp = temp.next;
@@ -66,32 +67,35 @@ class StoreContact {
         }
         prev.next = temp.next;
     }
-    public void search(String name){
+
+    public void search(String name) {
         String srch;
-        if(this.head == null){
+        if (this.head == null) {
             throw new IndexOutOfBoundsException();
         }
-        if(this.head.name.equals(name)){
-            srch = "Name: "+this.head.name+" Ph Number: "+this.head.phNo+" Email:"+this.head.email;
-            System.out.println(srch);
-        }else{
+        if (this.head.name.equals(name)) {
+            srch = "Name - " + this.head.name + " Ph Number - " + this.head.phNo + " Email - " + this.head.email;
+            Log.info(srch);
+        } else {
             Contact temp = head;
-            while(!temp.name.equals(name)){
-                if(temp.next!=null)
-                temp = temp.next;
+            while (!temp.name.equals(name)) {
+                if (temp.next != null) {
+                    temp = temp.next;
+                }
             }
-            srch ="Name: "+temp.name+" Ph Number: "+temp.phNo+" Email:"+temp.email;
-            System.out.println(srch);
+            srch = "Name: " + temp.name + " Ph Number: " + temp.phNo + " Email:" + temp.email;
+            Log.info(srch);
         }
 
     }
-    public void display(){
+
+    public void display() {
         Contact temp = head;
         String print;
-        while(temp!=null){
-            print = "Name: "+temp.name+" Ph Number: "+temp.phNo+" Email:"+temp.email;
+        while (temp != null) {
+            print = "Name-> " + temp.name + " Ph Number-> " + temp.phNo + " Email-> " + temp.email;
             temp = temp.next;
-            System.out.println(print);
+            Log.info(print);
         }
     }
 
@@ -99,45 +103,54 @@ class StoreContact {
 
 public class ContactsMain {
     public static final Logger Log = Logger.getLogger("InfoLogging");
+
     public static void main(String[] args) {
         StoreContact con = new StoreContact();
         Scanner input = new Scanner(System.in);
-        int flag =0;
-        do{
-            System.out.println("1.Add contact 2.Delete contact 3.Search 4.display 5.Exit");
+        int flag = 0;
+        do {
+            Log.info("1.Add contact 2.Delete contact 3.Search 4.display 5.Exit");
             int choice = input.nextInt();
-            switch(choice){
-                case 1:{
-                    System.out.println("Enter Name:");
+            switch (choice) {
+                case 1: {
+                    Log.info("Enter Name:");
                     String name = input.next();
-                    System.out.println("Enter Number");
+                    Log.info("Enter Number");
                     int phNo = input.nextInt();
-                    System.out.println("Enter E-mail:");
+                    Log.info("Enter E-mail:");
                     String email = input.next();
                     con.addContact(name, phNo, email);
-                }break;
-                case 2:{
-                    System.out.println("1.Delete last contact 2.Delete Number");
+                }
+                    break;
+                case 2: {
+                    Log.info("1.Delete last contact 2.Delete Number");
                     int op = input.nextInt();
-                    if(op==1){
+                    if (op == 1) {
                         con.delete();
+                    } else if (op == 2) {
+                        Log.info("Enter Number");
+                        String name = input.next();
+                        con.delete(name);
                     }
-                    else if(op==2){
-                        System.out.println("Enter Number");
-                        int phNo = input.nextInt();
-                        con.delete(phNo);
-                    }
-                }break;
+                }
+                    break;
                 case 3: {
-                    System.out.println("Enter Name: ");
+                    Log.info("Enter Name: ");
                     String name = input.next();
                     con.search(name);
-                }break;
-                case 4: con.display();break;
-                case 5: flag =1; break;
+                }
+                    break;
+                case 4:
+                    con.display();
+                    break;
+                case 5:
+                    flag = 1;
+                    break;
+                default:
+                    Log.info("Enter Correct choice");
             }
 
-        }while(flag == 0);
+        } while (flag == 0);
 
     }
 }
